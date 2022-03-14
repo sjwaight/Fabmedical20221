7bf32e34fc0a0d973776b0ca9944720106adf7c0
Simon Waight <simon.waight@gmail.com>
2022-03-14 06:14:42 +0000

    App insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("7057735e-2b45-4e90-96ba-675258bad06c");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
